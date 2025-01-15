303c25b7cd27e77a4a48bb28ee6b10eba9043253
syauqibilfaqih <alisyauqibilfaqih@gmail.com>
2025-01-15 16:35:48 +0100

    callibration and ensure value unit accelerometer

--- src/IoT_Project.cpp
+++ src/IoT_Project.cpp
@@ -16,6 +16,41 @@
 
 #define ICM20600_ADDR 0x69  // Default I²C address
 
+// Calibration offsets
+float ax_offset = 0;
+float ay_offset = 0;
+float az_offset = 0;
+
+// Function to read raw accelerometer data
+void readAccelerometer(int16_t &ax, int16_t &ay, int16_t &az) {
+    Wire.beginTransmission(ICM20600_ADDR);
+    Wire.write(0x3B);  // Starting register for accelerometer data
+    Wire.endTransmission(false);
+    Wire.requestFrom(ICM20600_ADDR, 6);  // Request 6 bytes (X, Y, Z)
+    ax = (Wire.read() << 8) | Wire.read();
+    ay = (Wire.read() << 8) | Wire.read();
+    az = (Wire.read() << 8) | Wire.read();
+}
+
+// Function to calibrate accelerometer
+void calibrateAccelerometer() {
+    int num_samples = 1000;
+    int16_t ax, ay, az;
+    long ax_sum = 0, ay_sum = 0, az_sum = 0;
+
+    for (int i = 0; i < num_samples; i++) {
+        readAccelerometer(ax, ay, az);
+        ax_sum += ax;
+        ay_sum += ay;
+        az_sum += az;
+        delay(10);  // Delay to allow for stable readings
+    }
+
+    ax_offset = ax_sum / num_samples;
+    ay_offset = ay_sum / num_samples;
+    az_offset = az_sum / num_samples;
+}
+
 // MQTT client and callback setup
 void callback(char* topic, byte* payload, unsigned int length);
 MQTT client("test.mosquitto.org", 1883, callback);
@@ -69,6 +104,10 @@ char* compress(int val){
   return newVal;
 } 
 
+// Function to convert raw accelerometer data to m/s²
+float convertToMS2(int16_t rawValue, float sensitivity) {
+    return (rawValue / sensitivity) * 9.80665;
+}
 
 
 // Let Device OS manage the connection to the Particle Cloud
@@ -101,6 +140,9 @@ void setup() {
     Wire.write(0x6B);  // Power management register
     Wire.write(0x00);  // Wake up
     Wire.endTransmission();
+
+    // Calibrate accelerometer
+    calibrateAccelerometer();
 }
 
 // loop() runs over and over again, as quickly as it can execute.
@@ -112,15 +154,28 @@ void loop() {
     // char message[15]="ub"; //ub**+**+**+**+q
 
     // Read accelerometer data
-    Wire.beginTransmission(ICM20600_ADDR);
-    Wire.write(0x3B);  // Starting register for accelerometer data
+    // Wire.beginTransmission(ICM20600_ADDR);
+    // Wire.write(0x3B);  // Starting register for accelerometer data
 
-    Wire.endTransmission(false);
-    Wire.requestFrom(ICM20600_ADDR, 6);  // Request 6 bytes (X, Y, Z)
+    // Wire.endTransmission(false);
+    // Wire.requestFrom(ICM20600_ADDR, 6);  // Request 6 bytes (X, Y, Z)
 
-    int16_t ax = (Wire.read() << 8) | Wire.read();
-    int16_t ay = (Wire.read() << 8) | Wire.read();
-    int16_t az = (Wire.read() << 8) | Wire.read();
+    // int16_t ax = (Wire.read() << 8) | Wire.read();
+    // int16_t ay = (Wire.read() << 8) | Wire.read();
+    // int16_t az = (Wire.read() << 8) | Wire.read();
+    int16_t ax, ay, az;
+    readAccelerometer(ax, ay, az);
+
+    // Apply calibration offsets
+    ax -= ax_offset;
+    ay -= ay_offset;
+    az -= az_offset;
+
+    // Convert raw values to m/s²
+    float ax_ms2 = convertToMS2(ax, 16384.0); // Assuming ±2g range
+    float ay_ms2 = convertToMS2(ay, 16384.0); // Assuming ±2g range
+    float az_ms2 = convertToMS2(az, 16384.0); // Assuming ±2g range
+
     
     // // always out of heap memory, idk why
     // strcat(message,compress(forceSensorAnalogMeasurement));
@@ -134,11 +189,11 @@ void loop() {
     message.concat(";");
     message.concat(forceSensorAnalogMeasurement);
     message.concat(";");
-    message.concat(ax);
+    message.concat(ax_ms2);
     message.concat(";");
-    message.concat(ay);
+    message.concat(ay_ms2);
     message.concat(";");
-    message.concat(az);
+    message.concat(az_ms2);
     message.concat(";");
     message.concat("e");
 
